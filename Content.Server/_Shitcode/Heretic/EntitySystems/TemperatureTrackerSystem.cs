74a67ee9138c5e31ee905443b10afd531050bc10
deltanedas <39013340+deltanedas@users.noreply.github.com>
2026-03-26 12:18:16 +0000

    fix void ritual shitcode not working in space (#1594)
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Server/_Shitcode/Heretic/EntitySystems/TemperatureTrackerSystem.cs
+++ Content.Server/_Shitcode/Heretic/EntitySystems/TemperatureTrackerSystem.cs
@@ -1,5 +1,6 @@
 using Content.Server.Atmos.EntitySystems;
 using Content.Shared._Shitcode.Heretic.Components;
+using Content.Shared.Atmos;
 using Content.Shared.Temperature;
 using Robust.Shared.Timing;
 
@@ -7,6 +8,7 @@ namespace Content.Server._Shitcode.Heretic.EntitySystems;
 
 public sealed class TemperatureTrackerSystem : EntitySystem
 {
+    [Dependency] private readonly AtmosphereSystem _atmos = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
 
     public override void Initialize()
@@ -14,6 +16,24 @@ public sealed class TemperatureTrackerSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<TemperatureTrackerComponent, AtmosExposedUpdateEvent>(OnAtmosExposed);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        // set them to CMB while in space, AtmosExposedUpdateEvent doesn't get raised if you are in space
+        var query = EntityQueryEnumerator<TemperatureTrackerComponent>();
+        var now = _timing.CurTime;
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (now < comp.NextUpdate)
+                continue;
+
+            comp.NextUpdate = now + comp.UpdateDelay;
+            var temp = _atmos.GetContainingMixture(uid)?.Temperature ?? Atmospherics.TCMB;
+            SetTemp((uid, comp), temp);
+        }
     }
 
     private void OnAtmosExposed(Entity<TemperatureTrackerComponent> ent, ref AtmosExposedUpdateEvent args)
@@ -24,6 +44,11 @@ public sealed class TemperatureTrackerSystem : EntitySystem
         ent.Comp.NextUpdate = _timing.CurTime + ent.Comp.UpdateDelay;
 
         var temp = args.GasMixture.Temperature;
+        SetTemp(ent, temp);
+    }
+
+    private void SetTemp(Entity<TemperatureTrackerComponent> ent, float temp)
+    {
         if (MathHelper.CloseToPercent(temp, ent.Comp.Temperature))
             return;
 
